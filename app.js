f94d00577ea6d817d6197624754581d733ae1f81
ABHISHEK SINGH <64076762+abk90133@users.noreply.github.com>
2020-06-16 14:02:33 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,48 @@
+//jsversion6
+
+const express = require("express");
+const bodyparser = require("body-parser");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.get("/", function(req, res){
+  var today = new Date();
+  var currentday = today.getDay();
+  var day = "";
+
+ switch(currentday) {
+   case 0:
+    day="Sunday";
+    break;
+   case 1:
+    day="Monday";
+    break;
+   case 2:
+    day="Tueday";
+    break;
+   case 3:
+    day="Wednesday";
+    break;
+    case 4:
+     day="Thursday";
+     break;
+    case 5:
+     day="Friday";
+     break;
+    case 6:
+     day="Saturday";
+     break;
+    default:
+    console.log("Error: Current Day is :" + currentday);
+ }
+  res.render("lists", {kindofday: day});
+
+});
+
+
+
+app.listen(3201, function(){
+  console.log("Server started on port 3201");
+});
